4836e904cc02f95b3f99408494d017fe0f12d282
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2025-12-26 15:55:59 -0500

    On shopping list form submit add new item from the form into the shopping list

--- react_shopping_list/src/ShoppingList.jsx
+++ react_shopping_list/src/ShoppingList.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+// ShoppingListForm submits data into ShoppingList
+import ShoppingListForm from "./ShoppingListForm";
+
 import ShoppingListItem from "./ShoppingListItem";
 
-export default function ShoppingLIst({ items }) {
+
+export default function ShoppingList() {
+
+  // items is an array of objects
+  const [items, setItems] = useState([
+    // for now these 2 items are hardcoded in the state
+    {id: uuidv4(), product: "bananas", quantity: 10},
+    {id: uuidv4(), product: "milk", quantity: 2}
+  ]);
+
+  const addItem=(item) => {
+    return setItems((currItems) => [...currItems, {...item, id: uuidv4()}]);
+  }
+
   return (
-    <ul>
-      {items.map((i) => (
-        <ShoppingListItem
-          obj={{...i}}
-          key={i.id}
-        />
-      ))}
-    </ul>
+    <div className="ShoppingList" style={{fontSize: '40px'}}>
+      <h1 style={{fontSize: '50px', color: "salmon"}}>Shopping List</h1>
+      <ul>
+        <ShoppingListForm addItem={addItem} />
+        {items.map((i) => <li key={i.id}>{i.product} - {i.quantity}</li>)}
+      </ul>
+    </div>
+    
   );
 }
